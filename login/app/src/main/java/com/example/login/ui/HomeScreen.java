662b9f2e94d938d4aa3175a2f8080a9c1196fff9
Mayuresh-loves-cpp <mvshinde640@gmail.com>
2020-11-01 15:23:33 +0530

    removed older project

--- login/app/src/main/java/com/example/login/ui/HomeScreen.java
+++ login/app/src/main/java/com/example/login/ui/HomeScreen.java
@@ -1,9 +1,10 @@
-package com.example.login;
+package com.example.login.ui.login;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.login.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
