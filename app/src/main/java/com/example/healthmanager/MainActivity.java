a6889c91efd36187641fb89f4a2b30f091ba8385
Mayuresh-loves-cpp <mvshinde640@gmail.com>
2020-11-24 22:39:44 +0530

    pushed new mp source

---
+++ app/src/main/java/com/example/healthmanager/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.healthmanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.net.Network;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDrawerLayout  = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        NavigationView nvDrawer = (NavigationView) findViewById(R.id.nv);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(nvDrawer);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void selectDrawerItem(MenuItem menuItem){
+        Fragment myFragment = null;
+        Class fragmentClass;
+        TextView textView = findViewById(R.id.txthome);
+        textView.setVisibility(View.GONE);
+        switch (menuItem.getItemId()){
+            case  R.id.homeScreen : fragmentClass = Home.class;
+                break;
+            case R.id.takesurvey : fragmentClass = TakeSurvey.class;
+                break;
+            case R.id.predictissues : fragmentClass = HealthIssues.class;
+                break;
+            case R.id.settings : fragmentClass = Settings.class;
+                break;
+            case R.id.help : fragmentClass = Help.class;
+                break;
+            case R.id.about : fragmentClass = About.class;
+                break;
+            default: fragmentClass = Home.class;
+                break;
+        }
+        try {
+            myFragment = (Fragment) fragmentClass.newInstance();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flcontent, myFragment).commit();
+        menuItem.setChecked(true);
+        //setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectDrawerItem(item);
+                return true;
+            }
+        });
+    }
+}
